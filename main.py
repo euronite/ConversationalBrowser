e915c2facb905af87a0877fb63750bc8870029a9
Leo <2314838k@student.gla.ac.uk>
2020-11-17 15:37:44 +0000

    empty caller id when not selecting caller id button fix in displayGraph click

--- main.py
+++ main.py
@@ -14,16 +14,87 @@ from conversationalbrowser import graph
 from pathlib import Path
 
 
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        self.model = Model()
+        self.callerIds = CallerModel()
+        super(MainWindow, self).__init__(*args, **kwargs)
+        # Load the UI
+        ui_location = Path(
+            "conversationalbrowser/ui/main_window.ui"
+        )  # This converts \ on windows etc.
+        uic.loadUi(ui_location, self)
+        graph.initmpl(self)
+
+    def openCallerDialog(self):
+        caller_dialog = CallerDialog(self)
+        caller_dialog.populateListView(self.model.callerIds)
+        if not self.callerIds.selected:
+            caller_dialog.selectAll()
+        else:
+            for pair in self.callerIds.selected:
+                # Selects the rows that have been selected previously. If the previously selected row is 0, selectall
+                if pair[1] == 0:
+                    caller_dialog.selectAll()
+                    break
+                caller_dialog.callerListWidget.item(pair[1]).setSelected(True)
+        caller_dialog.exec_()
+        caller_dialog.show()
+        self.callerIds.selected = []
+        for item in caller_dialog.callerListWidget.selectedItems():
+            # This appends the caller ID and the position in the list to the caller model.
+            self.callerIds.selected.append((item.text(), caller_dialog.callerListWidget.row(item)))
+
+    @pyqtSlot()
+    def browseSlot(self):
+        file_dialog = FileDialog()
+        file = file_dialog.openFileDialog()
+        if file:
+            self.model.set_file_name(file)
+            self.model.set_caller_ids(dm.get_all_call_ids(self.model.get_file_content()))
+
+            QMessageBox.information(self, "Load File", "File loaded successfully.")
+        else:
+            QMessageBox.critical(
+                self, "Load File Error", "File not selected, please try again"
+            )
+
+    @pyqtSlot()
+    def clearGraphSlot(self):
+        graph.rmmpl(self)
+
+    @pyqtSlot()
+    def displayCallerDialog(self):
+        self.openCallerDialog()
+
+    @pyqtSlot()
+    def displayGraphSlot(self):
+        if not self.callerIds.selected:
+            self.callerIds.selected = [('All Caller IDs', 0)]
+        if self.occurrencesRadioBtn.isChecked():
+            print("occurrences")
+        else:
+            print("duration")
+        if self.averageCheckbox.isChecked():
+            print("Average selected")
+        gender = self.genderDropdown.currentText()
+        role = self.roleDropdown.currentText()
+        cue = self.cueDropdown.currentText()
+        caller_id = self.callerIds.selected
+        chart = self.chartDropdown.currentText()
+        print(gender, role, cue, caller_id, chart)
+
+
 class FileDialog(QWidget):
     def __init__(self):
         super(FileDialog, self).__init__()
-        self.init_UI()
+        self.initUI()
 
-    def init_UI(self):
+    def initUI(self):
         self.setWindowTitle("Choose csv file")
         self.setGeometry(100, 100, 640, 480)
 
-    def open_file_dialog(self):
+    def openFileDialog(self):
         options = QtWidgets.QFileDialog.Options()
         file, _ = QtWidgets.QFileDialog.getOpenFileName(
             None,
@@ -62,75 +133,6 @@ class CallerDialog(QDialog):
     @pyqtSlot()
     def clearCallerList(self):
         self.deselectAll()
-
-
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, *args, **kwargs):
-        self.model = Model()
-        self.callerIds = CallerModel()
-        super(MainWindow, self).__init__(*args, **kwargs)
-        # Load the UI
-        ui_location = Path(
-            "conversationalbrowser/ui/main_window.ui"
-        )  # This converts \ on windows etc.
-        uic.loadUi(ui_location, self)
-        graph.initmpl(self)
-
-    def openCallerDialog(self):
-        caller_dialog = CallerDialog(self)
-        caller_dialog.populateListView(self.model.callerIds)
-        if not self.callerIds.selected:
-            caller_dialog.selectAll()
-        else:
-            for pair in self.callerIds.selected:
-                # Selects the rows that have been selected previously. If the previously selected row is 0, selectall
-                if pair[1] == 0:
-                    caller_dialog.selectAll()
-                    break
-                caller_dialog.callerListWidget.item(pair[1]).setSelected(True)
-        caller_dialog.exec_()
-        caller_dialog.show()
-        self.callerIds.selected = []
-        for item in caller_dialog.callerListWidget.selectedItems():
-            # This appends the caller ID and the position in the list to the caller model.
-            self.callerIds.selected.append((item, caller_dialog.callerListWidget.row(item)))
-
-    @pyqtSlot()
-    def browseSlot(self):
-        file_dialog = FileDialog()
-        file = file_dialog.open_file_dialog()
-        if file:
-            self.model.set_file_name(file)
-            self.model.set_caller_ids(dm.get_all_call_ids(self.model.get_file_content()))
-
-            QMessageBox.information(self, "Load File", "File loaded successfully.")
-        else:
-            QMessageBox.critical(
-                self, "Load File Error", "File not selected, please try again"
-            )
-
-    @pyqtSlot()
-    def clearGraphSlot(self):
-        graph.rmmpl(self)
-
-    @pyqtSlot()
-    def displayCallerDialog(self):
-        self.openCallerDialog()
-
-    @pyqtSlot()
-    def displayGraphSlot(self):
-        if self.occurrencesRadioBtn.isChecked():
-            print("occurrences")
-        else:
-            print("duration")
-        if self.averageCheckbox.isChecked():
-            print("Average selected")
-        gender = self.genderDropdown.currentText()
-        role = self.roleDropdown.currentText()
-        cue = self.cueDropdown.currentText()
-        #caller_id = self.callerDropdown.currentText()
-        chart = self.chartDropdown.currentText()
-        #print(gender, role, cue, caller_id, chart)
 
 
 def main():
